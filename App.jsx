1126dcd50bd358e2ac5b3259238bddad86d0852c
Johrot02 <sagoyjohan@gmail.com>
2025-06-19 01:34:27 +0200

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,5 @@
+// Hovedfil for LOOP-appens frontend
+function App() {
+  return <div>LOOP App</div>;
+}
+export default App;
---
+++ README.txt
@@ -0,0 +1,4 @@
+Instruksjoner:
+1. Last opp denne ZIP-filen til public_html
+2. Pakk den ut
+3. Gå til ditt domene
